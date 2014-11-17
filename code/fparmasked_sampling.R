24c78237d06fb7ddf86053166fc0bbac99fc72a7
Jiehua Chen <j@qe-design.com>
2014-11-17 00:17:22 -0800

    save sample points as 4 csv files or 2 csv files to have smaller files.

--- code/fparmasked_sampling.R
+++ code/fparmasked_sampling.R
@@ -1,24 +1,39 @@
-library(rgdal)
-library(downloader)
+# Generate GeoSurvey sampling points in Africa on a 1 km grid
+# J. Chen, W. Wu & M. Walsh, November, 2014
 
-download("https://www.dropbox.com/s/6wk1dmz8xxo6vdw/fAPAR.zip?dl=0", "fAPAR.zip", mode="wb")
-unzip("fAPAR.zip", overwrite=T)
+require(downloader)
+require(rgdal)
 
-fpar_ave <- readGDAL("fPAR_ave.tif", silent=TRUE)
-coordinates_fpar <- coordinates(fpar_ave)[!is.na(fpar_ave@data$band1), ]
-coordinates_fpar  <- coordinates_fpar[fpar_ave@data$band1[!is.na(fpar_ave@data$band1)]>0.5, ]
-coordinates_fpar_selected <- sample(1:dim(coordinates_fpar)[1], 1000000)
-coordinates_fpar_selected <- coordinates_fpar[coordinates_fpar_selected, ]
-coordinates_fpar_selected <- project(coordinates_fpar_selected, "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs", inv = TRUE)
+# Download sampling mask grid ---------------------------------------------
+download("https://www.dropbox.com/s/brikq1ceek16w3s/Af_mask.tif.zip?dl=0", "AF_mask.tif.zip", mode="wb")
+unzip("Af_mask.tif.zip", overwrite=T)
+mask <- readGDAL("AF_mask.tif", silent=TRUE)
 
+# Generate (n) sample and (v) validation locations ------------------------
+n <- 1000000
+v <- n/4
+set.seed(1385321)
+coord_mask <- coordinates(mask)[!is.na(mask@data$band1), ]
+mask_n <- sample(1:dim(coord_mask)[1], n)
+coord_mask_n <- coord_mask[mask_n, ]
+coord_mask_n <- project(coord_mask_n, "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs", inv = TRUE)
+coord_mask_v <- sample(1:n, v)
+coord_mask_v <- coord_mask_n[coord_mask_v, ]
 
-coordinates_fpar_doublecheck <- sample(1:1000000, 500000)
-coordinates_fpar_doublecheck <- coordinates_fpar_selected[coordinates_fpar_doublecheck, ]
+# Write csv's -------------------------------------------------------------
+k <- 4
+nk <- n/k
+for(i in 1:k){
+	write.csv(data.frame(Lon=coord_mask_n[(((i-1)*nk+1):(i*nk)),1], Lat=coord_mask_n[(((i-1)*nk+1):(i*nk)),2]), paste("AF_GS_sample_4p_", i, ".csv", sep=""), row.names=FALSE, quote=FALSE)
+}
 
+k <- 2
+nk <- n/k
+for(i in 1:k){
+	write.csv(data.frame(Lon=coord_mask_n[(((i-1)*nk+1):(i*nk)),1], Lat=coord_mask_n[(((i-1)*nk+1):(i*nk)),2]), paste("AF_GS_sample_2p_", i,".csv", sep=""), row.names=FALSE, quote=FALSE)
+}
 
-write.csv(data.frame(longitude=coordinates_fpar_selected[, 1], latitude=coordinates_fpar_selected[,2]), "1m_fparmasked_sampled.csv", row.names=FALSE, quote=FALSE)
-write.csv(data.frame(longitude=coordinates_fpar_doublecheck[, 1], latitude=coordinates_fpar_doublecheck[,2]), "1m_fparmasked_doublecheck.csv", row.names=FALSE, quote=FALSE)
-
+write.csv(data.frame(Lon=coord_mask_v[,1], Lat=coord_mask_v[,2]), "AF_GS_valpts.csv", row.names=FALSE, quote=FALSE)
 
 
 
