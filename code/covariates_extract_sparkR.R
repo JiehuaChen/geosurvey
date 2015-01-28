7787cd1c09807f8565086fdbda71aab9b5b1a24a
Jiehua Chen <j@qe-design.com>
2015-01-28 15:25:04 -0800

    sparkR code for remote sensing covariates extraction is added.

--- code/covariates_extract_sparkR.R
+++ code/covariates_extract_sparkR.R
@@ -28,28 +28,36 @@ if(length(args) < 2){
 cvpar = readLines(args[[2]])
 
 library(raster)
-glist <- list.files(path="../../../remotesensing_data/Af_grids_std", pattern="tif", full.names=T)
+glist <- list.files(path="../../remotesensing_data/Af_grids_std", pattern="tif", full.names=T)
 grid <- stack(glist)
 
 #Initialize Spark context 
 sc <- sparkR.init(args[[1]], "geosurvey_predict", sparkEnvir=list(spark.executor.memory="1g"))
 
-cv_bart <- function(indata) {
+geosurvey_data <- function(indata) {
    library(raster)
    print("In cv_bart")
    print(indata)
-   params <- strsplit(indata, split=",")[[1]]
-   extract_data <- extract(grid, cbind(as.numeric(params[1]), as.numeric(params[2])))
-   myres = paste(paste(params[1],params[2], sep=","), paste(extract_data, collapse=","), sep=",")
+   if(indata!=" "){
+       filename <- indata
+       locs <- read.csv(filename)
+       locs <- cbind(locs$x, locs$y)
+       extract_data <- extract(grid, locs)
+       extract_data <- cbind(locs, extract_data)
+       colnames(extract_data) <- c("Longitude", "Latitude",  names(grid)) 
+       write.csv(extract_data, paste(filename, "output", sep=""), row.names=FALSE)
+       myres = paste("finished", filename, sep=":")
+   }
 }
 
 nslices = 1
 if(length(args)==3) {nslices = as.numeric(args[[3]])}
 
 rdd <- parallelize(sc, coll=cvpar, numSlices = nslices)
-myerr <- flatMap(rdd,cv_bart)
+myerr <- flatMap(rdd,geosurvey_data)
 output <- collect(myerr)
-results <- t(mapply("as.numeric",  strsplit(do.call("rbind", output), split=",")))
-colnames(results) <- c("Longitude", "Latitude",  names(grid))
+
 # write.csv(results, "results.csv", row.names=FALSE)
+
+
 
