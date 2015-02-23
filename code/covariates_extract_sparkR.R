6e5e319ca3d900eebb2dfdedef07a21096d2b02c
Jiehua Chen <j@qe-design.com>
2015-02-23 14:05:39 +0300

    input cleaning data script is added.

--- code/covariates_extract_sparkR.R
+++ code/covariates_extract_sparkR.R
@@ -13,7 +13,6 @@ options(warn=2)
 
 library(SparkR)
 
-
 # read in command arguments
 args <- commandArgs(trailingOnly=TRUE)
 if(length(args) < 2){
@@ -31,6 +30,22 @@ library(raster)
 glist <- list.files(path="../../remotesensing_data/Af_grids_std", pattern="tif", full.names=T)
 grid <- stack(glist)
 
+
+tif.info <- GDALinfo(glist[1], silent=TRUE)
+totalrows <- tif.info[1]
+totalcols <- tif.info[2]
+bandsnum <- tif.info[3]
+origin.x <- tif.info[4]
+res.x <- tif.info[6]
+res.y <- tif.info[7]
+if(attr(tif.info, "ysign")==-1){
+	#origin of data array should be the left upper corner
+	origin.y <- tif.info[5] + totalrows*res.y
+}else{
+	origin.y <- tif.info[5]	
+}
+
+
 #Initialize Spark context 
 sc <- sparkR.init(args[[1]], "geosurvey_predict", sparkEnvir=list(spark.executor.memory="1g"))
 
@@ -40,12 +55,29 @@ geosurvey_data <- function(indata) {
    print(indata)
    if(indata!=" "){
        filename <- indata
-       locs <- read.csv(filename)
-       locs <- cbind(locs$x, locs$y)
-       extract_data <- extract(grid, locs)
-       extract_data <- cbind(locs, extract_data)
-       colnames(extract_data) <- c("Longitude", "Latitude",  names(grid)) 
-       write.csv(extract_data, paste(filename, "output", sep=""), row.names=FALSE)
+       geosurvey_data <- read.csv(filename)
+       names_geosurvey <- names(geosurvey_data)
+       locs <- cbind(geosurvey_data$x, geosurvey_data$y)
+       pixeln.x <- ceiling((locs[,1]-origin.x)/res.x)-1
+       pixeln.y <- ceiling((origin.y-locs[,2])/res.y)-1
+       neighbor.x <- c(-1, 0, 1, -1,0,1, -1, 0, 1)
+       neighbor.y <- c(-1, -1, -1, 0, 0, 0, 1, 1, 1)
+       neighbor_locations_gid  <-  matrix(NA, dim(locs)[1], 18)
+
+       for(i in 1:9){
+           neighbor_locations_gid[, (2*i-1)] <- (pixeln.x+neighbor.x[i])*res.x + origin.x + res.x/2
+           neighbor_locations_gid[, (2*i)] <- origin.y - ((pixeln.y+neighbor.y[i])*res.y + res.y/2)
+       }
+       
+       names_neighbors <- c("sw" ,"s", "se", "w","loc", "e", "nw", "n", "ne")
+
+       for(i in 1:9){
+           extract_data <- extract(grid, neighbor_locations_gid[, (2*i-1):(2*i)])
+           geosurvey_data <- cbind(geosurvey_data, extract_data)
+           print(i)
+       }
+       colnames(geosurvey_data) <- c(names_geosurvey, paste(names(grid), rep(names_neighbors, each=length(names(grid))), sep="_"))
+       write.csv(geosurvey_data, paste(filename, "output", sep=""), row.names=FALSE)
        myres = paste("finished", filename, sep=":")
    }
 }
